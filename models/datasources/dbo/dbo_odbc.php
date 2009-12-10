fdeea8cba4808802b1a7db6698eb01b6d6f49d10
predominant <graham@grahamweldon.com>
2009-12-11 09:59:29 +1100

    Update @package and @subpackage comments.

--- models/datasources/dbo/dbo_odbc.php
+++ models/datasources/dbo/dbo_odbc.php
@@ -12,9 +12,9 @@
  *
  * @copyright     Copyright 2005-2009, Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @package       cake
- * @subpackage    cake.cake.libs.model.dbo
- * @since         CakePHP(tm) v 0.10.5.1790
+ * @package       datasources
+ * @subpackage    datasources.models.datasources.dbo
+ * @since         CakePHP Datasources v 0.1
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
